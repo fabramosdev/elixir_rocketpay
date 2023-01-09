b0b93ec4e5b2072e28e0260ed89df45af5dd7744
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2023-01-09 12:58:21 -0300

    first commit

---
+++ lib/rocketpay/numbers.ex
@@ -0,0 +1,18 @@
+defmodule Rocketpay.Numbers do
+  def sum_from_file(filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Stream.map(&(String.to_integer/1))
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+  defp handle_file({:error, reason}), do: {:error, %{message: "Invalid  file! ((Erro: #{reason}))"}}
+end

95ab80644d24e56fe21556965ec59d4a3b0f2f41
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2023-01-19 18:08:23 -0300

    added tests

--- mix.exs
+++ mix.exs
@@ -11,7 +11,14 @@ defmodule Rocketpay.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      xref: [exclude: [Bcrypt]]
+      xref: [exclude: [Bcrypt]],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -47,7 +54,8 @@ defmodule Rocketpay.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:bcrypt_elixir, "~> 3.0"},
-      {:decimal, "~> 2.0"}
+      {:decimal, "~> 2.0"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,39 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Rocketpay.{Account, User}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Fabiano",
+      password: "123456",
+      nickname: "fsr",
+      email: "fabiano@mail.com",
+      age: 44
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} =
+      Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message:
+        "User Fabiano created with nickname 'fsr'. Use this nickname to make transactions in App",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Fabiano",
+        nickname: "fsr"
+      }
+    }
+
+    assert response == expected_response
+  end
+end
